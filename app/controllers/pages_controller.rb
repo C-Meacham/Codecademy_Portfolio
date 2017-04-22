4720f66284ee93df2de0c89542897b3f8ff91f74
codecademy <ccuser@codecademy.com>
2017-04-22 18:37:07 +0000

    Added link functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def welcome
   end
+  def contact
+  end
+  def work
+  end
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
+  get '/contact' => 'pages#contact'
+  get '/about' => 'pages#about'
+  get '/work' => 'pages#work'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
